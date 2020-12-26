5271b197ba60c5bb125b54430ac42ee5641b9201
Mauricio Melo <mauricioltw@gmail.com>
2020-12-26 15:42:12 -0300

    refactor(000): Use material ui components :hammer:

--- pages/graph.tsx
+++ pages/graph.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { TextareaAutosize } from '@material-ui/core';
-import { parseSpec } from '../services/specNetwork';
-import { adaptSpecs } from '../services/specToGraphLinkAdapter';
-import { ForceGraph } from '../components/ForceGraph';
+import React from "react";
+import { Grid, TextareaAutosize } from "@material-ui/core";
+import { parseSpec } from "../services/specNetwork";
+import { adaptSpecs } from "../services/specToGraphLinkAdapter";
+import { ForceGraph } from "../components/ForceGraph";
 
 const SPEC_EXAMPLE = `
 A links to B
@@ -13,7 +13,7 @@ C links to A
 export default function SpecGraph() {
   const [spec, setSpec] = React.useState(SPEC_EXAMPLE);
 
-  const handleSpecChange = React.useCallback(e => {
+  const handleSpecChange = React.useCallback((e) => {
     const spec = e.target.value;
     setSpec(spec);
   }, []);
@@ -23,14 +23,19 @@ export default function SpecGraph() {
   ]);
 
   return (
-    <div>
-      <TextareaAutosize
-        onChange={handleSpecChange}
-        aria-label="empty textarea"
-        placeholder="Empty"
-        value={spec}
-      />
-      <ForceGraph {...forceGraphData} />
-    </div>
+    <Grid container spacing={2} direction="row">
+      <Grid item md={3}>
+        <TextareaAutosize
+          onChange={handleSpecChange}
+          aria-label="empty textarea"
+          placeholder="Empty"
+          value={spec}
+        />
+      </Grid>
+
+      <Grid item md={9}>
+        <ForceGraph {...forceGraphData} />
+      </Grid>
+    </Grid>
   );
 }
