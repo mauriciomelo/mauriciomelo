fbb491baa6b6c8c794e8865b2731bdc800f71be9
Mauricio Melo <mauricioltw@gmail.com>
2021-06-06 11:19:31 -0300

    remove storybook default examples

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,7 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../@(page|components)/**/*.stories.mdx",
+    "../@(page|components)/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
