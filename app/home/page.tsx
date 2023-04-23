60f20a9787658905d303456c4d02134276666c43
Mauricio Melo <mmelo@thoughtworks.com>
2023-04-23 00:51:02 -0300

    add title

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,13 +1,6 @@
-"use client";
-import Head from "next/head";
-
 export default function Home() {
   return (
     <div className="flex h-screen w-full flex-col bg-black pt-[200px] text-center text-white ">
-      <Head>
-        <title>Mauricio Melo</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <h1 className="mb-4 text-5xl font-bold md:text-8xl">Mauricio Melo</h1>
 
       <a className="text-fuchsia-500" href="https://github.com/mauriciomelo">
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import "../styles/globals.css";
 export default function Layout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Mauricio Melo</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   );
