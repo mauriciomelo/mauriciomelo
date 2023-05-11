54b550993465a50a6477de53da624dbe4a9ea427
Mauricio Melo <mmelo@thoughtworks.com>
2023-05-11 17:50:17 -0300

    handle image input

--- app/faceclip/FaceClip.tsx
+++ app/faceclip/FaceClip.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import "@mediapipe/face_detection";
 import "@tensorflow/tfjs-core";
@@ -8,55 +9,55 @@ import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 import * as d3 from "d3";
 import React from "react";
+import produce from "immer";
 
 const model = faceDetection.SupportedModels.MediaPipeFaceDetector;
 
 const estimationConfig = { flipHorizontal: false } as const;
 
-const imageList = ["/woman.png", "/boy.png"];
+const imageList = ["mona_lisa.jpg", "/woman.png", "/boy.png"];
+
+type ImageWithFaces = { img: HTMLImageElement; faces: faceDetection.Face[] };
 export function FaceClip() {
-  const imageRef = React.useRef<HTMLImageElement>();
+  const galleryRef = React.useRef();
   const detectorPromiseRef =
     React.useRef<Promise<faceDetection.FaceDetector>>();
-  const imageContainerRef = React.useRef<HTMLImageElement>();
-  const [imagePath, setImagePath] = React.useState(imageList[0]);
+  const imageContainerRef = React.useRef<HTMLImageElement[]>([]);
+  const [currentIndex, setCurrentIndex] = React.useState(0);
 
-  const [images, setImages] = React.useState<
-    { img: HTMLImageElement; faces: faceDetection.Face[] }[]
-  >([]);
+  const [images, setImages] = React.useState<ImageWithFaces[]>([]);
   const faces = React.useMemo(
     () =>
       images.flatMap(({ img, faces }) => faces.map((face) => ({ img, face }))),
     [images]
   );
 
+  const createImages = React.useCallback(async (urlList: string[]) => {
+    let resolvedImages: ImageWithFaces[] = [];
+
+    detectorPromiseRef.current = detectorPromiseRef.current || createDetector();
+    for (let url of urlList) {
+      const [img, detector] = await Promise.all([
+        createImageNodeFromURL(url),
+        detectorPromiseRef.current,
+      ]);
+      const faces = await detector.estimateFaces(img, estimationConfig);
+      resolvedImages.push({ img, faces });
+    }
+
+    return resolvedImages;
+  }, []);
+
   React.useEffect(() => {
     (async () => {
       try {
-        detectorPromiseRef.current =
-          detectorPromiseRef.current || createDetector();
-
-        let resolvedImages = [];
-
-        for (let imagePath of imageList) {
-          const [img, detector] = await Promise.all([
-            createImageNodeFromURL(imagePath),
-            detectorPromiseRef.current,
-          ]);
-
-          const faces = await detector.estimateFaces(img, estimationConfig);
-          resolvedImages.push({ img, faces });
-        }
-
-        setImages(resolvedImages);
+        setImages(await createImages(imageList));
       } catch (error) {
         console.error(error);
         setImages([]);
       }
     })();
-  }, [setImages]);
-
-  const hasFaces = Boolean(faces.length);
+  }, [setImages, createImages]);
 
   React.useEffect(() => {
     faces.forEach(({ face, img }, index) => {
@@ -66,7 +67,7 @@ export function FaceClip() {
       const scale = d3
         .scaleLinear()
         .range([0, 100])
-        .domain([0, boundingBoxStyle(face).width]);
+        .domain([0, expandFaceBox(face.box).width]);
 
       svg.attr("width", 150);
       svg.attr("height", 150);
@@ -74,89 +75,126 @@ export function FaceClip() {
       const imageNaturalWidth = img.naturalWidth;
 
       image.attr("width", `${scale(imageNaturalWidth)}px`);
-      image.attr("y", scale(-boundingBoxStyle(face).top));
-      image.attr("x", scale(-boundingBoxStyle(face).left));
+      image.attr("y", scale(-expandFaceBox(face.box).yMin));
+      image.attr("x", scale(-expandFaceBox(face.box).xMin));
     });
   }, [faces]);
 
+  const handleImageUpload = React.useCallback<
+    React.ChangeEventHandler<HTMLInputElement>
+  >(
+    (event) => {
+      const { files } = event.target;
+
+      if (files.length) {
+        const urlList = Array.from(files).map((file) =>
+          URL.createObjectURL(file)
+        );
+        createImages(urlList).then((newImages) => {
+          const updatedImages = images.concat(newImages);
+          setImages(updatedImages);
+        });
+      }
+    },
+    [createImages, images]
+  );
+
+  React.useEffect(() => {
+    // @ts-expect-error
+    galleryRef.current?.slideToIndex(images.length - 1);
+  }, [images]);
+
+  const currentImage = images[currentIndex];
+  const displayedImageNode = imageContainerRef.current[currentIndex];
+
   return (
-    <div className="flex h-[100vh] flex-col justify-between">
-      <h1>Face Clip</h1>
-
-      <div className="mt-8 flex  flex-col flex-wrap overflow-x-auto p-3">
-        {faces.map(({ img }, index) => (
-          <svg
-            key={index}
-            width="100mm"
-            height="100mm"
-            viewBox="0 0 100 100"
-            version="1.1"
-            id={`faceSvg-${index}`}
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs>
-              <clipPath id="clipPath">
-                <path
-                  style={{ fill: "#fff", strokeWidth: 0.411003 }}
-                  id="path408"
-                  d="m 61.134979,73.5244 c 0.479655,-0.276929 -48.765837,-0.276931 -48.286182,-2e-6 0.479655,0.276929 -24.143089,-42.37092 -24.143089,-41.817062 0,0.553858 24.622748,-42.093988 24.143093,-41.817059 -0.479655,0.2769284 48.765837,0.2769308 48.286182,2e-6 -0.479655,-0.276929 24.143089,42.370919 24.143089,41.817062 0,-0.553858 -24.622748,42.093988 -24.143093,41.817059 z"
-                  transform="matrix(1.0354929,0,0,1.0354929,11.695161,17.16728) "
-                />
-              </clipPath>
-            </defs>
-
-            {img && (
-              <image
-                width={`${img.getBoundingClientRect().width}px`}
-                clipPath="url(#clipPath)"
-                preserveAspectRatio="none"
-                xlinkHref={img.src}
-              />
-            )}
-          </svg>
+    <div>
+      {Boolean(currentImage?.faces.length && displayedImageNode) &&
+        currentImage.faces.map((face, faceIndex) => (
+          <div
+            key={`${currentIndex}-${faceIndex}`}
+            className="pointer-events-none absolute z-10 border-2 border-solid border-white"
+            style={boundingBoxStyle({
+              img: currentImage.img,
+              face,
+              imageNode: displayedImageNode,
+            })}
+          />
         ))}
-      </div>
-      <div className="flex h-[50vh]">
-        <ImageGallery
-          items={images.map(({ img, faces }) => ({
-            original: img.src,
-            img,
-            faces,
-            thumbnail: img.src,
-          }))}
-          onSlide={(index) => setImagePath(imageList[index])}
-          showFullscreenButton={false}
-          thumbnailPosition="top"
-          showThumbnails={false}
-          renderItem={(item) => {
-            const { img, faces } = item;
+      <div className="flex h-[100vh] flex-col justify-between">
+        <h1>Face Clip</h1>
 
-            return (
-              <div className="relative">
-                {Boolean(faces.length && imageContainerRef.current) &&
-                  faces.map((face, i) => (
-                    <div
-                      key={i}
-                      className="absolute border-2 border-solid border-white"
-                      style={boundingBoxStyle(
-                        face,
-                        d3
-                          .scaleLinear()
-                          .range([
-                            0,
-                            imageContainerRef.current.getBoundingClientRect()
-                              .width,
-                          ])
-                          .domain([0, img.naturalWidth])
-                      )}
-                    />
-                  ))}
+        <div className="mt-8 flex  flex-col flex-wrap overflow-x-auto p-3">
+          {faces.map(({ img }, index) => (
+            <svg
+              key={index}
+              width="100mm"
+              height="100mm"
+              viewBox="0 0 100 100"
+              version="1.1"
+              id={`faceSvg-${index}`}
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <clipPath id="clipPath">
+                  <path
+                    style={{ fill: "#fff", strokeWidth: 0.411003 }}
+                    id="path408"
+                    d="m 61.134979,73.5244 c 0.479655,-0.276929 -48.765837,-0.276931 -48.286182,-2e-6 0.479655,0.276929 -24.143089,-42.37092 -24.143089,-41.817062 0,0.553858 24.622748,-42.093988 24.143093,-41.817059 -0.479655,0.2769284 48.765837,0.2769308 48.286182,2e-6 -0.479655,-0.276929 24.143089,42.370919 24.143089,41.817062 0,-0.553858 -24.622748,42.093988 -24.143093,41.817059 z"
+                    transform="matrix(1.0354929,0,0,1.0354929,11.695161,17.16728) "
+                  />
+                </clipPath>
+              </defs>
 
-                <img ref={imageContainerRef} src={item.original} />
-              </div>
-            );
-          }}
-        />
+              {img && (
+                <image
+                  width={`${img.getBoundingClientRect().width}px`}
+                  clipPath="url(#clipPath)"
+                  preserveAspectRatio="none"
+                  xlinkHref={img.src}
+                />
+              )}
+            </svg>
+          ))}
+        </div>
+
+        <div>
+          <input
+            type="file"
+            accept="image/*"
+            multiple
+            onChange={handleImageUpload}
+          />
+        </div>
+
+        <div className="flex">
+          <ImageGallery
+            ref={galleryRef}
+            items={images.map(({ img }, index) => ({
+              original: img.src,
+              thumbnail: img.src,
+              index,
+            }))}
+            onSlide={(index) => setCurrentIndex(index)}
+            showFullscreenButton={false}
+            thumbnailPosition="top"
+            showThumbnails={false}
+            renderItem={(item) => {
+              return (
+                <div className=" relative flex h-[50vh] w-[100vw] items-center justify-center ">
+                  <img
+                    ref={(element) =>
+                      (imageContainerRef.current[item.index] = element)
+                    }
+                    alt=""
+                    className="max-h-full object-contain"
+                    src={item.original}
+                  />
+                </div>
+              );
+            }}
+          />
+        </div>
       </div>
     </div>
   );
@@ -178,16 +216,39 @@ function createImageNodeFromURL(url: string) {
   });
 }
 
-function boundingBoxStyle(face: faceDetection.Face, scale = (x: number) => x) {
-  const scaleFace = true;
+function expandFaceBox(box: faceDetection.Face["box"]) {
+  return produce(box, (draft) => {
+    const width = draft.width * 2;
+    const height = draft.height * 2;
+    draft.width = width;
+    draft.height = height;
+    draft.xMin = draft.xMin - width / 4;
+    draft.yMin = draft.yMin - height / 3;
+  });
+}
 
-  const faceWidth = face.box.width * (scaleFace ? 2 : 1);
-  const faceHeight = face.box.height * (scaleFace ? 2 : 1);
+function boundingBoxStyle({
+  img,
+  face,
+  imageNode,
+}: {
+  face: faceDetection.Face;
+  img: ImageWithFaces["img"];
+  imageNode: HTMLImageElement;
+}) {
+  const imageNodeClientRect = imageNode.getBoundingClientRect();
+  const scale = d3
+    .scaleLinear()
+    .range([0, imageNodeClientRect.width])
+    .domain([0, img.naturalWidth]);
+
+  const expand = true;
+  const expandedBox = expand ? expandFaceBox(face.box) : face.box;
 
   return {
-    width: scale(faceWidth),
-    height: scale(faceHeight),
-    left: scale(face.box.xMin - (scaleFace ? faceWidth / 4 : 0)),
-    top: scale(face.box.yMin - (scaleFace ? faceHeight / 3 : 0)),
+    width: scale(expandedBox.width),
+    height: scale(expandedBox.height),
+    left: scale(expandedBox.xMin) + imageNodeClientRect.left,
+    top: scale(expandedBox.yMin) + imageNodeClientRect.top,
   };
 }
