5e56c2bf53b404f9f1fae55db58f1df82b2e3fb3
Mauricio Melo <mauricioltw@gmail.com>
2023-07-24 18:43:59 -0300

    init webrtc demo page

---
+++ app/webrtc/page.jsx
@@ -0,0 +1,7 @@
+export default function WebRTCPage() {
+  return (
+    <div className="flex h-screen w-full flex-col bg-black pt-[200px] text-center text-white ">
+      <h1 className="mb-4 text-5xl font-bold md:text-8xl">WebRTC Demo</h1>
+    </div>
+  );
+}
