ff891fabb44fb98c6028d4fb039a96e47b452d60
Mauricio Melo <mauricioltw@gmail.com>
2023-07-29 16:21:36 -0300

    refactor webrtc demo

--- app/webrtc/page.tsx
+++ app/webrtc/page.tsx
@@ -1,44 +1,26 @@
 "use client";
-import { Box, Button, ButtonGroup, Container } from "@mui/material";
+import { Box, Button, ButtonGroup, Container, Typography } from "@mui/material";
 import React from "react";
 
 const FILE_EXTENSION = "myapp";
 
 export default function WebRTCPage() {
-  const [connected, setConnected] = React.useState(false);
   const [counter, setCounter] = React.useState<number>(0);
-  const localConnection = React.useRef<RTCPeerConnection>();
-  const dataChannel = React.useRef<RTCDataChannel>();
-  const [localDescription, setLocalDescription] =
-    React.useState<RTCSessionDescription>();
 
   const answerInviteRef = React.useRef<HTMLInputElement>();
 
-  function handleDataChannelStatusChange(event) {
-    if (!dataChannel.current) {
-      return;
-    }
-
-    const state = dataChannel.current.readyState;
-    console.log("DataChannelStatus", state);
-
-    if (state === "open") {
-      setConnected(true);
-    }
-  }
-
-  function handleIceConnectionStateChange() {
-    const state = localConnection.current.iceConnectionState;
-    console.log("IceConnectionState", state);
-  }
-
-  function handleIceCandidate(event: RTCPeerConnectionIceEvent) {
-    if (event.candidate) {
-      return;
-    }
-
-    setLocalDescription(localConnection.current.localDescription);
-  }
+  const {
+    localConnection,
+    dataChannel,
+    localDescription,
+    createOffer,
+    createAnswer,
+    connected,
+  } = useWebRTC({
+    onMessage(event) {
+      setCounter((count) => count + parseInt(event.data));
+    },
+  });
 
   function handleInviteToConnect() {
     createOffer();
@@ -62,60 +44,18 @@ export default function WebRTCPage() {
     }
   }, [localDescription]);
 
-  const assignChannel = (channel: RTCDataChannel) => {
-    channel.onopen = handleDataChannelStatusChange;
-    channel.onmessage = handleMessage;
-    channel.onclose = handleDataChannelStatusChange;
-    dataChannel.current = channel;
-  };
-
-  const createOffer = () => {
-    assignChannel(localConnection.current.createDataChannel("dataChannel"));
-
-    localConnection.current.createOffer().then((description) => {
-      localConnection.current.setLocalDescription(description);
-    });
-  };
-
-  const createAnswer = () => {
-    localConnection.current.ondatachannel = (event) => {
-      assignChannel(event.channel);
-    };
-
-    localConnection.current.createAnswer().then((description) => {
-      localConnection.current.setLocalDescription(description);
-    });
-  };
-
-  const handleMessage = (event: MessageEvent) => {
-    setCounter((count) => count + parseInt(event.data));
-  };
-
-  React.useEffect(() => {
-    localConnection.current = new RTCPeerConnection();
-
-    localConnection.current.oniceconnectionstatechange =
-      handleIceConnectionStateChange;
-
-    localConnection.current.onicecandidate = handleIceCandidate;
-
-    return () => {
-      console.log("Closing connection");
-      if (dataChannel.current) {
-        dataChannel.current.close();
-      }
-      localConnection.current.close();
-    };
-  }, []);
-
   const handleAnswerInvite = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const file = event.target.files[0];
+    const file = event.target?.files?.[0];
 
+    if (!file) {
+      console.error("No invite file found");
+      return;
+    }
     const answer = new RTCSessionDescription(JSON.parse(await file.text()));
 
-    localConnection.current.setRemoteDescription(answer);
+    localConnection?.setRemoteDescription(answer);
 
     if (answer.type === "offer") {
       createAnswer();
@@ -123,43 +63,164 @@ export default function WebRTCPage() {
 
     console.log("uploaded", answer);
   };
-  const handleChangeCounter = (number) => () => {
-    dataChannel.current.send(number);
+  const handleChangeCounter = (number) => {
+    dataChannel?.send(number);
     setCounter((count) => count + number);
   };
 
   return (
-    <Container>
-      <div className="flex h-screen w-full flex-col bg-black pt-[200px] text-center text-white ">
-        <h1 className="mb-4 text-5xl font-bold md:text-8xl">WebRTC Demo</h1>
-        <Button
-          variant="outlined"
-          onClick={handleInviteToConnect}
-          sx={{ mb: 4 }}
-        >
-          Invite to connect
-        </Button>
-        <Button
-          variant="outlined"
-          onClick={() => {
-            answerInviteRef.current.click();
-          }}
-        >
-          Answer Invite
-        </Button>
-        <Box sx={{ visibility: "hidden" }}>
-          <input
-            ref={answerInviteRef}
-            type="file"
-            accept={`.${FILE_EXTENSION}`}
-            onChange={handleAnswerInvite}
-          />
-        </Box>
-        {connected ? "Connected" : null}
-        Counter: {counter}
-        <Button onClick={handleChangeCounter(1)}>+1</Button>
-        <Button onClick={handleChangeCounter(10)}>+10</Button>
-      </div>
+    <Container
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Typography variant="h3" component="h1" sx={{ my: 8 }}>
+        WebRTC Demo
+      </Typography>
+      <Button variant="outlined" onClick={handleInviteToConnect} sx={{ mb: 4 }}>
+        Invite to connect
+      </Button>
+      <Button
+        variant="outlined"
+        onClick={() => {
+          answerInviteRef.current?.click();
+        }}
+      >
+        Answer Invite
+      </Button>
+      <Box
+        component="input"
+        sx={{ visibility: "hidden" }}
+        ref={answerInviteRef}
+        type="file"
+        accept={`.${FILE_EXTENSION}`}
+        onChange={handleAnswerInvite}
+      />
+      {connected ? (
+        <>
+          <Typography sx={{ color: "success.main" }}>Connected</Typography>
+
+          <Counter value={counter} onChange={handleChangeCounter} />
+        </>
+      ) : null}
     </Container>
   );
+}
+
+function Counter({
+  onChange,
+  value,
+}: {
+  value: number;
+  onChange: (number) => void;
+}) {
+  return (
+    <>
+      <Typography sx={{ my: 4 }}>Counter: {value}</Typography>
+      <ButtonGroup>
+        <Button onClick={() => onChange(1)}>+1</Button>
+        <Button onClick={() => onChange(10)}>+10</Button>
+      </ButtonGroup>
+    </>
+  );
+}
+
+function useWebRTC({
+  onMessage,
+}: {
+  onMessage: (event: MessageEvent) => void;
+}) {
+  const [connected, setConnected] = React.useState(false);
+
+  const [localConnection, setLocalConnection] =
+    React.useState<RTCPeerConnection>();
+  const [dataChannel, setDataChannel] = React.useState<RTCDataChannel | null>(
+    null
+  );
+
+  const [localDescription, setLocalDescription] =
+    React.useState<RTCSessionDescription | null>();
+
+  const handleDataChannelStatusChange = React.useCallback(function (event) {
+    const dataChannel = this;
+
+    const state = dataChannel.readyState;
+    console.log("DataChannelStatus", state);
+
+    if (state === "open") {
+      setConnected(true);
+    }
+  }, []);
+
+  const assignDataChannelCallbacks = React.useCallback(
+    (channel: RTCDataChannel) => {
+      channel.onopen = handleDataChannelStatusChange;
+      channel.onclose = handleDataChannelStatusChange;
+      channel.onmessage = onMessage;
+    },
+    [handleDataChannelStatusChange, onMessage]
+  );
+
+  const createOffer = React.useCallback(() => {
+    if (!localConnection) {
+      return;
+    }
+
+    const channel = localConnection.createDataChannel("dataChannel");
+    assignDataChannelCallbacks(channel);
+    setDataChannel(channel);
+
+    localConnection.createOffer().then((description) => {
+      localConnection.setLocalDescription(description);
+    });
+  }, [assignDataChannelCallbacks, localConnection]);
+
+  const createAnswer = React.useCallback(() => {
+    if (!localConnection) {
+      console.error("no local connection");
+      return;
+    }
+
+    localConnection.createAnswer().then((description) => {
+      localConnection.setLocalDescription(description);
+    });
+  }, [localConnection]);
+
+  React.useEffect(() => {
+    const connection = new RTCPeerConnection();
+    connection.signalingState;
+    connection.oniceconnectionstatechange = () => {
+      const state = connection.iceConnectionState;
+      console.log("IceConnectionState", state);
+    };
+
+    connection.ondatachannel = (event) => {
+      assignDataChannelCallbacks(event.channel);
+      setDataChannel(event.channel);
+    };
+
+    connection.onicecandidate = (event) => {
+      if (event.candidate) {
+        return;
+      }
+      setLocalDescription(connection?.localDescription);
+    };
+    setLocalConnection(connection);
+
+    return () => {
+      console.log("Closing connection");
+      connection.close();
+    };
+  }, []);
+
+  return {
+    localConnection,
+    dataChannel,
+    createOffer,
+    createAnswer,
+    localDescription,
+    connected,
+  };
 }
